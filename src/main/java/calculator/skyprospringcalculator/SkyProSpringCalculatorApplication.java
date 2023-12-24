d7e1982c3ed53044ef9e39fc5d69a5cf4355308b
Khal01 <aliyev_56@list.ru>
2023-12-24 15:39:15 +0300

    calculator

---
+++ src/main/java/calculator/skyprospringcalculator/SkyProSpringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package calculator.skyprospringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProSpringCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProSpringCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/calculator/skyprospringcalculator/SkyProSpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package calculator.skyprospringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
